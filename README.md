8c0bf7116accce31ebfd3870331fc9ed52cf6181
Karn Wong <kahnwong@Karns-MacBook-Air.local>
2017-05-04 13:38:04 +0700

    Updated the code

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # calibre-covers-extractor
-A short Python script that grab covers location and save them in another location
+A short Python script that grab covers from ```$directory``` and save them in another location.
 
 # Usage
 
-Change **book_directory** to where the covers are. Edit **destination_directory** to where you want to store the covers *PLUS* its filename (digits will be appended later). For example, **"D:\z-covers\RESULTS\cover"** copies files as cover0.jpg, cover1.jpg and so on.
+Change **book_directory** to where the covers are. Edit **destination_directory** to where you want to store the covers.
 
 # Notes
 
--- calibre_cover_extrator.py
+++ calibre_cover_extrator.py
@@ -1,25 +1,18 @@
 import os
 import shutil
 
-book_directory = "D:\OneDrive\BOOKS\Fiction\Gerald Seymour"
-destination_directory = "D:\z-covers\RESULTS\cover"
-
-#~ def get_covers(directory):
+book_directory = "/Users/kahnwong/Dropbox/Library/Fiction/Ake Edwardson"
+destination_directory = "/Users/kahnwong/Dropbox/"
 
 cover_paths = []
 
 for root, directories, files in os.walk(book_directory):
-    
     for filename in files:
         if filename.endswith('.jpg'):
             filepath = os.path.join(root, filename)
             cover_paths.append(filepath)
-						
-	#~ return cover_paths
-	
-#~ def copy_covers(cover_paths):
+            print(filepath)
+
 for index, cover in enumerate(cover_paths):
     shutil.copy(cover, destination_directory + str(index) + '.jpg')
-			
-
-
+print('----DONE----')
